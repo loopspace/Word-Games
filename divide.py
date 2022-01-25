d2948f1383e76a7e703701a589ed9f30a5ce0b04
Andrew Stacey <loopspace@mathforge.org>
2022-01-25 21:02:34 +0000

    Divide-and-conquer strategy
    This runs the divide-and-conquer strategy to find solutions for each
    word.

---
+++ divide.py
@@ -0,0 +1,49 @@
+from puzzle import Puzzle
+
+wordle = Puzzle("Wordle", 5, True, 'simplewords', 'simplewords')
+wordle.load_scores('scores')
+
+guesses = {}
+n = 0
+first_guess = wordle.get_next_divide_guess()
+# Iterate through the possible words we can guess
+for w in wordle.guessable_words:
+    # Keep track of how many words we've done
+    n += 1
+    # Reset the scores
+    wordle.reset_scores()
+    # Initialise with the first guess, as it is always the same
+    m = 1
+    g = first_guess
+    wordle.apply_guess(g, wordle_score(w,g))
+    # Show where we're at
+    print(f"\rWord: {w} ({n}) guess {m}", end='')
+    # While we haven't solved it
+    while not wordle.is_solved():
+        # Increment the guess counter
+        m += 1
+        # Get the next guess
+        g = wordle.get_next_divide_guess()
+        # Apply it
+        wordle.apply_guess(g, wordle_score(w,g))
+        # Say where we are
+        print(f"\rWord: {w} ({n}) guess {m}", end='')
+    # The puzzle is solved, in that there's only one word left
+    # If it *wasn't* our current guess, we need one more guess to say what it is
+    if g != w:
+        m += 1
+        print(f"\rWord: {w} ({n}) guess {m}", end='')
+    # Save the number of guesses
+    guesses[w] = m
+    print("\n", end='')
+
+guess_order = {k: v for k, v in sorted(guesses.items(), key=lambda item: -item[1])}
+
+for k in list(guess_order.keys())[:10]:
+    print(f'{k} {guess_order[k]}')
+
+number_of_guesses = [0]*10
+for k,v in guesses.items():
+    number_of_guesses[v] += 1
+
+print(number_of_guesses)
