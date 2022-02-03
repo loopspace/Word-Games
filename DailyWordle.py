382d8353bec99b8873b1cad0980a2db827364bb9
Andrew Stacey <loopspace@mathforge.org>
2022-02-03 22:40:54 +0000

    Fixed-list strategy and first guesses hard-coded
    Included a fixed-list strategy which uses a single list of words to
    guess with, and hard-coded the initial guesses of the other strategies
    to speed up the code (since those are always the same).

--- DailyWordle.py
+++ DailyWordle.py
@@ -11,38 +11,67 @@ def int_to_score(n):
 
 wordle = Puzzle("Wordle", 5, True, 'wordle_guessable_words', 'wordle_guessing_words')
 
-wordle_day = datetime.now().timetuple().tm_yday + 195
+wordle_day = datetime.now().timetuple().tm_yday + 195 + 1
 
 daily_wordle = wordle.guessable_words[wordle_day]
 
 print('Frequency Analysis')
-f = 0
-while not wordle.is_solved():
-    guess = wordle.get_next_frequency_guess()
+f = 1
+guess = "soare"
+while True:
     score = wordle.score(daily_wordle,guess)
     print(f' {guess}: {" ".join([str(s) for s in int_to_score(score)])}')
     wordle.apply_guess(guess, score)
+
+    if wordle.is_solved():
+        break
+    guess = wordle.get_next_frequency_guess()
     f += 1
 
 if guess != daily_wordle:
     f += 1
 
+wordle.reset_scores()
+
+guess_list = ['soare', 'linty', 'caber', 'whelp', 'vodka', 'feign', 'amaze', 'torus', 'adobe']
+
+print('Naive Frequency Analysis')
+n = 0
+for guess in guess_list:
+    score = wordle.score(daily_wordle,guess)
+    print(f' {guess}: {" ".join([str(s) for s in int_to_score(score)])}')
+    wordle.apply_guess(guess, score)
+    n += 1
+    if wordle.is_solved():
+        break
+
+if wordle.is_solved():
+    if guess != daily_wordle:
+        n += 1
+else:
+    n = "unsolved"    
 
 wordle.reset_scores()
 
 print('Minmax Analysis')
-m = 0
-while not wordle.is_solved():
-    (guess,division) = wordle.get_next_divide_guess()
+m = 1
+guess = "arise"
+division = 168
+while True:
     score = wordle.score(daily_wordle,guess)
-    print(f' {guess} ({division}): {" ".join([str(s) for s in int_to_score(score)])}')
+    pad = " " * (3 - len(str(division)))
+    print(f' {guess} {pad}({division}): {" ".join([str(s) for s in int_to_score(score)])}')
     wordle.apply_guess(guess, score)
+    if wordle.is_solved():
+        break
+    (guess,division) = wordle.get_next_divide_guess()
     m += 1
 
 if guess != daily_wordle:
     m += 1
 
 print("Daily Wordle Ranking")
-print(f'Frequency: {f}')
-print(f'   Minmax: {m}')
+print(f' Frequency: {f}')
+print(f'Guess List: {n}')
+print(f'    Minmax: {m}')
 
